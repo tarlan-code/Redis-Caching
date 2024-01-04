d587ca343e09950af7ba6776e003f0b3b38a4507
tarlan-code <tu769djc3@code.edu.az>
2024-01-04 16:58:57 +0400

    Redis Pub/Sub

---
+++ RedisPubSub.Subscriber/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = redis.GetSubscriber();
+
+////direct
+//await subscriber.SubscribeAsync("mychannel", (channel, msg) =>
+//{
+//    Console.WriteLine(msg);
+//});
+
+
+//pattern maching
+await subscriber.SubscribeAsync("mychannel.*", (channel, msg) =>
+{
+    Console.WriteLine(msg);
+});
+
+Console.Read(); 
---
+++ RedisPubSub/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber publisher = redis.GetSubscriber();
+
+////direct
+//while (true)
+//{
+//    Console.Write("Mesaj:");
+
+//    string mesaj = Console.ReadLine();
+
+//    await publisher.PublishAsync("mychannel", mesaj); 
+//}
+
+
+//pattern maching
+while (true)
+{
+    Console.Write("Mesaj:");
+
+    string mesaj = Console.ReadLine();
+
+    await publisher.PublishAsync("mychannel.google", mesaj);
+}
