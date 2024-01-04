d2ffce6994213e2f29677be0589c88d053eab43d
tarlan-code <tu769djc3@code.edu.az>
2024-01-04 15:52:53 +0400

    first commit

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+
+        [HttpPost("set")]
+        public async Task<IActionResult> Set(string name,string surname)
+        {
+            await _distributedCache.SetStringAsync("name", name, options: new(){
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname), options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+            string name = await _distributedCache.GetStringAsync("name");
+            byte[] surnameByte = await _distributedCache.GetAsync("surname");
+
+            if (String.IsNullOrEmpty(name) & surnameByte is null)
+            {
+                return NotFound();
+            }
+
+            string surname = surnameByte is null ? "" : Encoding.UTF8.GetString(surnameByte);
+            
+            return Ok(name + " " + surname);
+        } 
+    }
+}
---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Set(string key = "name",string value = null)
+        {
+            if (string.IsNullOrEmpty(value))
+                return BadRequest("Value is not null");
+            
+            _memoryCache.Set(key, value);
+
+            return Created();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string key)
+        {
+            if(_memoryCache.TryGetValue<string>(key, out string name))
+                return Ok(name);
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string key)
+        {
+            _memoryCache.Remove(key);
+
+            return Ok();    
+        }
+
+
+        [HttpPost("SetDate")]
+        public async Task<IActionResult> SetDate()
+        {
+            _memoryCache.Set<DateTime?>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(15),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+
+            return Created();
+        }
+
+        [HttpGet("Getdate")]
+        public async Task<IActionResult> Getdate()
+        {
+            if (_memoryCache.TryGetValue<DateTime>("date", out DateTime date))
+                return Ok(date);
+
+            return NotFound();
+        }
+
+    }
+}
